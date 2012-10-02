7a51d863a93fcd438492a8e84c877d00a9742dd0
Jason Larke <20924425@student.uwa.edu.au>
2012-10-02 22:49:30 +0800

    Finished first template integration and added all of the static pages. Contact page still needs to be completed.

---
+++ protected/views/site/static/comingevents.php
@@ -0,0 +1,31 @@
+<div id="templatemo_main">
+	<div id="content">
+		<h2>Coming Events</h2>
+		<h4>AGM 2012</h4>
+		<p>
+			On Wednesday the 19th of September we invite you to dinner at 6 o'clock at Drabble House with our traditional pea soup and shooters. 
+			To facilitate catering, please let us know by next Friday (the 14th) if you able to attend. 
+			Of course, you are still welcome to the meeting even if you don't let us know, but an RSVP would be helpful. 
+			The AGM begins at 7:30 when we will review the past year and look forward to the coming year.
+		</p>
+		<p>
+			<em>Important:</em> If you are not going to the AGM, please nominate a proxy to ensure that we get a quorum to enable us to hold a meeting.  
+			To nominate a proxy, simply e-mail me your name and specify your nominee (either someone specific or "the chairperson").
+		</p>
+		<p>
+			<em>Note:</em>  Membership forms will be available at the meeting, so you can renew your membership then.
+		</p>
+	</div>	
+	
+	<div id="sidebar">
+		<h3>Important Documents</h3>
+		<ul class="templatemo_list">
+			<li><a href="<?php echo Yii::app()->baseUrl; ?>/docs/2012_AGM_Agenda.pdf" target="_blank">2012 AGM Agenda</a></li>
+			<li><a href="<?php echo Yii::app()->baseUrl; ?>/docs/2012_AGM_Annual_Report.pdf" target="_blank">2012 AGM Annual Report</a></li>
+			<li><a href="<?php echo Yii::app()->baseUrl; ?>/docs/2012_AGM_Projected_Plan.pdf" target="_blank">2012 AGM Projected Plan</a></li>
+			<li><a href="<?php echo Yii::app()->baseUrl; ?>/docs/AGM_Minutes.pdf" target="_blank">2011 AGM Minutes</a></li>
+		</ul>
+	</div> <!-- end of sidebar -->
+
+	<div class="cleaner"></div>
+</div>
---
+++ protected/views/site/static/emigration.php
@@ -0,0 +1,89 @@
+<div id="templatemo_main">
+	<div id="content">		
+		<h3 id="Visas">Visas</h3>		
+		<div class="about_box">
+			<img src="<?php echo Yii::app()->baseUrl; ?>/images/immi.jpg" alt="Dept. of Immigration and Citizenship" />
+			<h4>Department of Immigration and Citizenship</h4>
+			<p class="position"><a href="http://www.immi.gov.au/">Visit Website</a></p>
+			<p>For enquiries regarding visiting, working, studying or moving to Australia, we refer to the state website from the Department of Immigration and Citizenship.</p>
+			<div class="cleaner"></div>
+		</div>
+		<div class="about_box">
+			<img src="<?php echo Yii::app()->baseUrl; ?>/images/wag.jpg" alt="Government of Western Australia" />
+			<h4>Government of Western Australia</h4>
+			<p class="position"><a href="http://www.migration.wa.gov.au/Pages/LivingInWesternAustralia.aspx">Visit Website</a></p>
+			<div class="cleaner"></div>
+		</div>
+		
+		
+		<h3 id="Jobs">Jobs</h3>
+		<p>It is best if you have a profession that is on the list of those needed in Australia: <a href="http://www.immi.gov.au/skilled/sol/">Skilled Migration</a></p>
+		<p>You can apply for jobs at the following places:</p>
+		
+		<div class="about_box">
+			<img src="<?php echo Yii::app()->baseUrl; ?>/images/mycareer.jpg" alt="My Career" />
+			<h4>My Career</h4>
+			<p class="position"><a href="http://mycareer.com.au/">Visit Website</a></p>
+			<div class="cleaner"></div>
+		</div>
+		<div class="about_box">
+			<img src="<?php echo Yii::app()->baseUrl; ?>/images/careerone.jpg" alt="Career One" />
+			<h4>Career One</h4>
+			<p class="position"><a href="http://www.careerone.com.au/">Visit Website</a></p>
+			<div class="cleaner"></div>
+		</div>
+		<div class="about_box">
+			<img src="<?php echo Yii::app()->baseUrl; ?>/images/seek.jpg" alt="Seek" />
+			<h4>Seek</h4>
+			<p class="position"><a href="http://www.seek.com.au/">Visit Website</a></p>
+			<div class="cleaner"></div>
+		</div>
+
+		<h3 id="Help">Additional Help</h3>
+		<p>On the internet there are lots of different websites about Western Australia and about to move here. </p>
+		<p>Some examples of such websites are: </p>	
+				
+		<div class="about_box">
+			<img src="<?php echo Yii::app()->baseUrl; ?>/images/westernaustralia.jpg" alt="Western Australia" />
+			<h4>Western Australia</h4>
+			<p class="position"><a href="http://www.westernaustralia.com/au/Pages/Welcome_to_Western_Australia.aspx">Visit Website</a></p>
+			<div class="cleaner"></div>
+		</div>
+		<div class="about_box">
+			<img src="<?php echo Yii::app()->baseUrl; ?>/images/aussiemove.jpg" alt="Aussie Move" />
+			<h4>Aussie Move</h4>
+			<p class="position"><a href="http://www.aussiemove.com/">Visit Website</a></p>
+			<div class="cleaner"></div>
+		</div>
+		<div class="about_box">
+			<img src="<?php echo Yii::app()->baseUrl; ?>/images/bcl.jpg" alt="BCL" />
+			<h4>BCL</h4>
+			<p class="position"><a href="http://www.bcl.com.au/perth/movetoperth/default.htm">Visit Website</a></p>
+			<div class="cleaner"></div>
+		</div>
+			<div class="about_box">
+			<img src="<?php echo Yii::app()->baseUrl; ?>/images/movetoperth.jpg" alt="Move To Perth" />
+			<h4>Move To Perth</h4>
+			<p class="position"><a href="http://movetoperth.net/">Visit Website</a></p>
+			<div class="cleaner"></div>
+		</div>
+			<div class="about_box">
+			<img src="<?php echo Yii::app()->baseUrl; ?>/images/pos.jpg" alt="POS" />
+			<h4>Kerstin Nivbrant</h4>
+			<p class="position">Perth Orientation Services</p>
+			<p><a href="mailto:kerstin@perthos.com.au">kerstin@perthos.com.au</a></p>
+			<div class="cleaner"></div>
+		</div>
+	</div>
+
+	<div id="sidebar">
+		<h3>Categories</h3>
+		<ul class="templatemo_list">
+			<li><a href="#Visas">Visa</a></li>
+			<li><a href="#Jobs">Jobs</a></li>
+			<li><a href="#Help">Additional Help</a></li>
+		</ul>
+	</div> <!-- end of sidebar -->
+
+	<div class="cleaner"></div>
+</div> 
