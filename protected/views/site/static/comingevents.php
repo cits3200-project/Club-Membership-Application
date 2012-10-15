56ddfd0b54b1dc3ddbb236b12945d3ccd96757f6
Gregory Paul Collin <20351726@student.uwa.edu.au>
2012-10-15 13:04:40 +0800

    Cosmetic changes to comingevents page.

--- protected/views/site/static/comingevents.php
+++ protected/views/site/static/comingevents.php
@@ -11,42 +11,42 @@ $this->breadcrumbs=array(
 		<a name="Svensk gudstj&auml;nst i Perth"></a>
 		<h4>Svensk gudstj&auml;nst i Perth</h4>
 		<p>
-			The Swedish vicar is coming to Perth.  A church service will be held as follows:
-			
-			DATE: Saturday 27 October
-			TIME: 2 pm
-			PLACE: St Johns Lutheran Church, 16 Aberdeen Street, Northbridge 
-
-			Coffee and cakes will be served after the service.  All welcome! 
-			Should you wish to book a baptism/christening, a wedding or a time to talk to the priest, then please contact Per.
+			The Swedish vicar is coming to Perth.  A church service will be held as follows:<br />
+			<br />
+			DATE: Saturday 27 October<br />
+			TIME: 2 pm<br />
+			PLACE: St Johns Lutheran Church, 16 Aberdeen Street, Northbridge <br />
+			<br />
+			Coffee and cakes will be served after the service.  All welcome! <br />
+			Should you wish to book a baptism/christening, a wedding or a time to talk to the priest, then please contact Per.<br />
 		</p>
-		<p>Per Axerup , Vicar and Dean:
-		   Mobile: 0418 510 640
-           Email: <a href="mailto:per.axerup@churchofsweden.org">per.axerup@churchofsweden.org</a>
-
-		   Karin Gruber: 
+		<p>Per Axerup , Vicar and Dean:<br />
+		   Mobile: 0418 510 640<br />
+           Email: <a href="mailto:per.axerup@churchofsweden.org">per.axerup@churchofsweden.org</a><br />
+		<br />
+		   Karin Gruber: <br />
 		   Mobile: 0408 092 013
 		</p>
-		<iframe width="940" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com.au/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=st+john's+lutheran+church+northbridge&amp;aq=&amp;sll=35.973972,-83.923517&amp;sspn=0.007484,0.013797&amp;ie=UTF8&amp;hq=st+john's+lutheran+church+northbridge&amp;hnear=&amp;radius=15000&amp;ll=-31.947782,115.861918&amp;spn=0.125604,0.220757&amp;t=m&amp;z=12&amp;iwloc=A&amp;cid=11681735246227746915&amp;output=embed"></iframe>
+		<iframe width="880" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com.au/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=st+john's+lutheran+church+northbridge&amp;aq=&amp;sll=35.973972,-83.923517&amp;sspn=0.007484,0.013797&amp;ie=UTF8&amp;hq=st+john's+lutheran+church+northbridge&amp;hnear=&amp;radius=15000&amp;ll=-31.947782,115.861918&amp;spn=0.125604,0.220757&amp;t=m&amp;z=12&amp;iwloc=A&amp;cid=11681735246227746915&amp;output=embed"></iframe>
 	
 		<a name="Adventsbazaaren"></a>
 		<h4>Adventsbazaaren</h4>
-		<p>If you wish to book a table, please send an e-mail to <a href="mailto:info@svenskaklubben.org.au">info@svenskaklubben.org.au</a> or call Peter on 0405 658 750.
-           Setup time begins at 2 o'clock, and tidy up concludes at 7 o'clock.
-			
-			DATE: Sunday 25 November
-			TIME: 3 pm - 6pm
+		<p>If you wish to book a table, please send an e-mail to <a href="mailto:info@svenskaklubben.org.au">info@svenskaklubben.org.au</a> or call Peter on 0405 658 750.<br />
+           Setup time begins at 2 o'clock, and tidy up concludes at 7 o'clock.<br />
+		<br />
+			DATE: Sunday 25 November<br />
+			TIME: 3 pm - 6pm<br />
 			PLACE: Shenton Park Main Hall 
 		</p>
-		<iframe width="940" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com.au/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=shenton+college&amp;aq=&amp;sll=-31.855398,115.795898&amp;sspn=0.502742,0.883026&amp;ie=UTF8&amp;hq=shenton+college&amp;ll=-31.962429,115.80278&amp;spn=0.031387,0.055189&amp;t=m&amp;z=14&amp;iwloc=A&amp;cid=4193991388409452396&amp;output=embed"></iframe>
+		<iframe width="880" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com.au/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=shenton+college&amp;aq=&amp;sll=-31.855398,115.795898&amp;sspn=0.502742,0.883026&amp;ie=UTF8&amp;hq=shenton+college&amp;ll=-31.962429,115.80278&amp;spn=0.031387,0.055189&amp;t=m&amp;z=14&amp;iwloc=A&amp;cid=4193991388409452396&amp;output=embed"></iframe>
 		
 		<a name="Lucia"></a>
 		<h4>Lucia</h4>
 		<p>
-			DATE: Saturday/Sunday 15/16 December
+			DATE: Saturday/Sunday 15/16 December<br />
 			PLACE: East Fremantle Oval
 		</p>
-		<iframe width="940" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com.au/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=east+fremantle+oval&amp;aq=&amp;sll=-31.955658,115.802765&amp;sspn=0.031387,0.055189&amp;ie=UTF8&amp;hq=east+fremantle+oval&amp;t=m&amp;z=14&amp;iwloc=A&amp;cid=1082254475369013312&amp;ll=-32.046205,115.768614&amp;output=embed"></iframe>
+		<iframe width="880" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com.au/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=east+fremantle+oval&amp;aq=&amp;sll=-31.955658,115.802765&amp;sspn=0.031387,0.055189&amp;ie=UTF8&amp;hq=east+fremantle+oval&amp;t=m&amp;z=14&amp;iwloc=A&amp;cid=1082254475369013312&amp;ll=-32.046205,115.768614&amp;output=embed"></iframe>
 	</div>
 	
 	<div id="sidebar">
